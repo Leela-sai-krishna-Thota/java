dbb56a0b10e17130c230012b7079e58efbedb8ca
Thota Leela Sai Krishna <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2025-03-28 18:53:21 +0530

    Update ADS.java

--- ADS.java
+++ ADS.java
@@ -570,6 +570,60 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]); printf("\nAverage Waiting Time -- %f", wtavg/n); printf("\nAverage Turnaround Time -- %f", tatavg/n);
 }
 
+job schedule
+import java.util.*;
 
+class GfG {
 
+    static ArrayList<Integer> jobSequencing(int[] id, 
+            int[] deadline, int[] profit) {
+        int n = id.length;
+        ArrayList<Integer> ans = new ArrayList<>(Arrays.asList(0, 0));
+
+        // pair the profit and deadline of
+        // all the jobs together
+        ArrayList<int[]> jobs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            jobs.add(new int[]{profit[i], deadline[i]});
+        }
+
+        // sort the jobs based on profit
+        // in decreasing order
+        jobs.sort((a, b) -> b[0] - a[0]);
+
+        // array to store result of job sequence
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+
+        for (int i = 0; i < n; i++) {
+            int start = Math.min(n, jobs.get(i)[1]) - 1;
+            for (int j = start; j >= 0; j--) {
+
+                // if slot is empty
+                if (result[j] == -1) {
+                    result[j] = i;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (result[i] != -1) {
+                ans.set(1, ans.get(1) + jobs.get(result[i])[0]);
+                ans.set(0, ans.get(0) + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] id = {1, 2, 3, 4, 5};
+        int[] deadline = {2, 1, 2, 1, 1};
+        int[] profit = {100, 19, 27, 25, 15};
+        ArrayList<Integer> ans = jobSequencing(id, deadline, profit);
+        System.out.println(ans.get(0) + " " + ans.get(1));
+    }
+}
+    
 
